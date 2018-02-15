78679c70cddd18acc8b2f5bbb062defece9274e9
AraceliGS <aragutarras@gmail.com>
2018-02-14 23:52:26 -0500

    añadiendo estilos

--- public/js/app.js
+++ public/js/app.js
@@ -11,26 +11,29 @@ $(document).ready(function () {
   });
 
   function bringInfo() {
-    debugger;
+    // debugger;
     var data = JSON.parse(this.responseText);
     console.log(data.results);
-    var character = data.results[0];
-    var name = character.name;
-    var gender = character.gender;
-    var height = character.height;
-    var birthYear = character.birth_year;
+    var character = data.results.forEach(function (el) {
+      var name = el.name;
+      var gender = el.gender;
+      var height = el.height;
+      var birthYear = el.birth_year;
+
+      var $container = document.createElement('div');
+      var $li = document.createElement('li');
+      $li.innerHTML = '<div><span>Name: </span><span>' + name + '</span></div><div><span>Gender: </span><span>' + gender + '</span></div><div><span>Height: </span><span>' + height + '</span></div><div><span>Birth Year: </span><span>' + birthYear + '</span></div>';
+      $container.append($li);
+      $container.className = 'character-container col s12 m6 l3';
+      $responseContainer.append($container);
+
+      return $container;
+    });
     /* const homeplanet = JSON.parse(this.open('https://swapi.co/api/planets/1/', sendHomeworldInfo));   
     const sendHomeworldInfo = (request, response) => {
       const data = request.name;
       response.send(data);
     }; */
-
-    var $container = document.createElement('div');
-    var $li = document.createElement('li');
-    $li.innerHTML = '<div><span>Name: </span><span>' + name + '</span></div><div><span>Gender: </span><span>' + gender + '</span></div><div><span>Height: </span><span>' + height + '</span></div><div><span>Birth Year: </span><span>' + birthYear + '</span></div>';
-    $container.append($li);
-    $container.className = 'character-container col s12 m6 l4';
-    $responseContainer.append($container);
   };
 
   function handleError() {
--- src/app.js
+++ src/app.js
@@ -12,23 +12,26 @@ $(document).ready(() => {
     // debugger;
     const data = JSON.parse(this.responseText);
     console.log(data.results);
-    const character = data.results[0];
-    const name = character.name;
-    const gender = character.gender;
-    const height = character.height;
-    const birthYear = character.birth_year;
+    let character = data.results.forEach(function(el) {
+      const name = el.name;
+      const gender = el.gender;
+      const height = el.height;
+      const birthYear = el.birth_year;
+
+      let $container = document.createElement('div');        
+      let $li = document.createElement('li');
+      $li.innerHTML = `<div><span>Name: </span><span>${name}</span></div><div><span>Gender: </span><span>${gender}</span></div><div><span>Height: </span><span>${height}</span></div><div><span>Birth Year: </span><span>${birthYear}</span></div>`;
+      $container.append($li);
+      $container.className = 'character-container col s12 m6 l3';
+      $responseContainer.append($container);
+
+      return $container;
+    });
     /* const homeplanet = JSON.parse(this.open('https://swapi.co/api/planets/1/', sendHomeworldInfo));   
     const sendHomeworldInfo = (request, response) => {
       const data = request.name;
       response.send(data);
     }; */
-
-    let $container = document.createElement('div');        
-    let $li = document.createElement('li');
-    $li.innerHTML = `<div><span>Name: </span><span>${name}</span></div><div><span>Gender: </span><span>${gender}</span></div><div><span>Height: </span><span>${height}</span></div><div><span>Birth Year: </span><span>${birthYear}</span></div>`;
-    $container.append($li);
-    $container.className = 'character-container col s12 m6 l4';
-    $responseContainer.append($container);
   };
 
   function handleError() {
