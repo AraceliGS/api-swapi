863c9e4acbad87b50089ee05dedbae1cf74425be
AraceliGS <aragutarras@gmail.com>
2018-02-15 22:45:16 -0500

    modificando ya añadiendo nuevos estilos a la vista register y principal

--- src/app.js
+++ src/app.js
@@ -19,15 +19,7 @@ $(document).ready(() => {
       }
     });
   });
-
-  $('#special-button').one('click', () => {
-    const characterInfoRequest = new XMLHttpRequest();
-    characterInfoRequest.open('GET', 'https://swapi.co/api/people/');
-    characterInfoRequest.onload = bringInfo;
-    characterInfoRequest.onerror = handleError;
-    characterInfoRequest.send();
-  });
-
+  
   function bringInfo(event) {
     // debugger;
     const data = JSON.parse(this.responseText);
@@ -42,13 +34,6 @@ $(document).ready(() => {
     // debugger;
     let $modal = `<div id="modal1" class="modal"><div class="modal-content"><h4>${name}</h4><ul><li><span>Gender: </span>${gender}</li><li><span>Height: </span>${height}</li><li><span>Birth Year: </span>${birthYear}</li></ul></div><div class="modal-footer">
     <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Cerrar</a></div</div>`;       
-    // // return $container;
-    // let $container = document.createElement('div');        
-    // let $li = document.createElement('li');
-    // $li.innerHTML = `<div><span>Name: </span><span>${name}</span></div><div><span>Gender: </span><span>${gender}</span></div><div><span>Height: </span><span>${height}</span></div><div><span>Birth Year: </span><span>${birthYear}</span></div>`;
-    // $container.append($li);
-    // $container.className = 'character-container col s12 m6 l3';
-    // $infoContainer.append($container);
     $infoContainer.append($modal);
     return $('#modal1').modal();
   };
