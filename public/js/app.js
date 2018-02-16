d36c35fb94967d0774a4300616a18f005a6a4f2c
AraceliGS <aragutarras@gmail.com>
2018-02-15 22:03:17 -0500

    añadiendo funcionalidad- aparición del modal

--- public/js/app.js
+++ public/js/app.js
@@ -3,27 +3,24 @@
 $(document).ready(function () {
   var $responseContainer = $('#response-container');
   var $infoContainer = $('#info-container');
-  var $array = [];
-  var $images = $('.starwars-image');
-  $images.each(function (el) {
-    debugger;
-    var $thisImage = $images[el];
-    $images[el].on('click', function () {
-      console.log($images[el]);
+  var $currentIndex = 0;
+  var $listOfImages = $('.starwars-image');
+  $listOfImages.on('click', function (event) {
+    $infoContainer.innerHTML = '';
+    event.preventDefault();
+    $listOfImages.each(function (el) {
+      if ($listOfImages[el] === event.target) {
+        var thisImage = $listOfImages[el];
+        var thisIndex = el;
+        $currentIndex = thisIndex;
+        var characterInfoRequest = new XMLHttpRequest();
+        characterInfoRequest.open('GET', 'https://swapi.co/api/people/');
+        characterInfoRequest.onload = bringInfo;
+        characterInfoRequest.onerror = handleError;
+        characterInfoRequest.send();
+      }
     });
   });
-
-  // let $infoModals = $array.map(function(el) {
-  //   $(`#${el}`).modal();
-  //   console.log('Soy yo');
-  //   el.on('click', function() {
-  //     const characterInfoRequest = new XMLHttpRequest();
-  //     characterInfoRequest.open('GET', 'https://swapi.co/api/people/');
-  //     characterInfoRequest.onload = bringInfo;
-  //     characterInfoRequest.onerror = handleError;
-  //     characterInfoRequest.send();
-  //   });
-  // }); 
 
   $('#special-button').one('click', function () {
     var characterInfoRequest = new XMLHttpRequest();
@@ -33,39 +30,29 @@ $(document).ready(function () {
     characterInfoRequest.send();
   });
 
-  function bringInfo() {
+  function bringInfo(event) {
     // debugger;
     var data = JSON.parse(this.responseText);
     console.log(data);
     console.log(data.results);
-    var character = data.results.forEach(function (el) {
-      var name = el.name;
-      var gender = el.gender;
-      var height = el.height;
-      var birthYear = el.birth_year;
-      // debugger;       
-      $('name').innerText = name;
-      $('gender').innerText = 'Gender: ';
-      $('gender-content').innerText = gender;
-      $('height').innerText = 'Height: ';
-      $('height-content').innerText = height;
-      $('birth-year').innerText = 'Birth Year: ';
-      $('birth-year-content').innerText = birthYear;
-
-      // // return $container;
-      // let $container = document.createElement('div');        
-      // let $li = document.createElement('li');
-      // $li.innerHTML = `<div><span>Name: </span><span>${name}</span></div><div><span>Gender: </span><span>${gender}</span></div><div><span>Height: </span><span>${height}</span></div><div><span>Birth Year: </span><span>${birthYear}</span></div>`;
-      // $container.append($li);
-      // $container.className = 'character-container col s12 m6 l3';
-      // $infoContainer.append($container);
-      return console.log('Ok');
-    });
+    var response = data.results;
+    var character = data.results[$currentIndex];
+    var name = character.name;
+    var gender = character.gender;
+    var height = character.height;
+    var birthYear = character.birth_year;
+    // debugger;
+    var $modal = '<div id="modal1" class="modal"><div class="modal-content"><h4>' + name + '</h4><ul><li><span>Gender: </span>' + gender + '</li><li><span>Height: </span>' + height + '</li><li><span>Birth Year: </span>' + birthYear + '</li></ul></div><div class="modal-footer">\n    <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Cerrar</a></div</div>';
+    // // return $container;
+    // let $container = document.createElement('div');        
+    // let $li = document.createElement('li');
+    // $li.innerHTML = `<div><span>Name: </span><span>${name}</span></div><div><span>Gender: </span><span>${gender}</span></div><div><span>Height: </span><span>${height}</span></div><div><span>Birth Year: </span><span>${birthYear}</span></div>`;
+    // $container.append($li);
+    // $container.className = 'character-container col s12 m6 l3';
+    // $infoContainer.append($container);
+    $infoContainer.append($modal);
+    return $('#modal1').modal();
   };
-
-  // function moreInfo(data) {
-
-  // };
 
   function handleError() {
     console.log('Se ha presentado un error');
